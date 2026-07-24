b4bfd464b9e1107e90496803c0623c3ec632fc62
Guest453 <victorromeojaime981@gmail.com>
2026-07-23 23:34:58 -0300

    add recovery updater manifest

---
+++ store/apps/recoveryupdater.app/manifest.lua
@@ -0,0 +1,12 @@
+return {
+  id="recoveryupdater",
+  name="recovery updater",
+  version="1.0.0",
+  author="idk os",
+  entry="main.lua",
+  icon="recoveryupdater",
+  color=0x397fca,
+  description="reboot into a ram-resident full-system updater",
+  package="idkos-recovery-updater",
+  files={"manifest.lua","main.lua","bridge.lua","updater.lua"}
+}
