11cfa25e7373ae7416ed2004fdde1b67221fba8c
Guest453 <victorromeojaime981@gmail.com>
2026-07-25 20:45:29 -0300

    add computronics music manifest

---
+++ store/apps/music.app/manifest.lua
@@ -0,0 +1,10 @@
+return {
+  id="music",
+  name="music",
+  version="1.0.0",
+  author="idk os",
+  entry="main.lua",
+  icon="music",
+  color=0x4ec7e8,
+  description="computronics sound-card music player and tiny synthesizer"
+}
