f18721787cced9b7780197192d4252c2c3dccde7
Guest453 <victorromeojaime981@gmail.com>
2026-07-25 21:06:21 -0300

    fix music package identity

--- store/apps/music.app/manifest.lua
+++ store/apps/music.app/manifest.lua
@@ -1,10 +1,12 @@
 return {
   id="music",
   name="music",
-  version="1.0.0",
+  version="1.0.1",
   author="idk os",
   entry="main.lua",
   icon="music",
   color=0x4ec7e8,
+  package="computronics-music-original",
+  files={"manifest.lua","main.lua"},
   description="computronics sound-card music player and tiny synthesizer"
 }
