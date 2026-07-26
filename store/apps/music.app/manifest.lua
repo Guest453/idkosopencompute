4f2b61656ce84593e1b575693e340f2111a40b92
Guest453 <victorromeojaime981@gmail.com>
2026-07-25 22:10:06 -0300

    publish multitrack music studio 2.0.0

--- store/apps/music.app/manifest.lua
+++ store/apps/music.app/manifest.lua
@@ -1,12 +1,12 @@
 return {
   id="music",
   name="music",
-  version="1.1.0",
+  version="2.0.0",
   author="idk os",
   entry="main.lua",
   icon="music",
   color=0x4ec7e8,
   package="computronics-music-original",
   files={"manifest.lua","main.lua"},
-  description="computronics music player, piano, and 16-step song maker"
+  description="computronics player and 4-track studio with named 256-step songs"
 }
