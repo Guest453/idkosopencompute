9281a665652d65f1777e3f051ccac0000f3e1f53
Guest453 <victorromeojaime981@gmail.com>
2026-08-02 02:55:05 -0300

    publish idk os update 8

---
+++ updates/latest.lua
@@ -0,0 +1,7 @@
+return {
+  version = 8,
+  file = "update8.os",
+  path = "updates/update8.os",
+  channel = "main",
+  notes = "automatic boot update checks, .os packages, diagnostic update environment, and kernel panic screen"
+}
